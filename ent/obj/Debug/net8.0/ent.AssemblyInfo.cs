98f17bf15948742c3d4438fb326824768dc73a5e
SaibotGames <mkbarczuk@wp.pl>
2025-05-18 23:10:23 +0200

    i understand kubernetes now

--- ent/obj/Debug/net8.0/ent.AssemblyInfo.cs
+++ ent/obj/Debug/net8.0/ent.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ent")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f9dac734e4a7490ee7571a677e8476c63dd99db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1cf0bd9c2af458b0e002134d90963cb3345f12b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ent")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ent")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
